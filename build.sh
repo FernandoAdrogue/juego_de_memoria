46c39b9789db394a053f8512662a098af30b5e16
chinoadrogue <chinoadrogue@gmail.com>
2025-10-01 14:25:13 +0000

    fix build

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 echo "Generando firebase-config.js..."
 cat > firebase-config.js << EOL
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-    authDomain: FIREBASE_AUTH_DOMAIN,
-      projectId: FIREBASE_PROJECT_ID,
-        storageBucket: FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-            appId: FIREBASE_APP_ID
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            appId: NEXT_PUBLIC_FIREBASE_APP_ID
             };
 EOL
 echo "firebase-config.js generado correctamente."
