86e11057e177d0e06c8cab1661e360d7e7a3a92b
chinoadrogue <chinoadrogue@gmail.com>
2025-10-02 21:23:18 +0000

    reestructuracion de carpetas

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+// import i18n from 'astro-i18n';
+
+// https://astro.build/config
+export default defineConfig({
+    // integrations: [
+    //     i18n({
+    //         primaryLocale: 'es',
+    //         secondaryLocales: ['en', 'pt'],
+    //         fallback: 'es',
+    //         localesDir: './src/locales'
+    //     })
+    // ]
+});
